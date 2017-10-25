e9c302582f9acfd6c3ccbeea5309755a3a42b804
Mike Bull <hello@mikebull.info>
2017-10-25 13:37:18 +0100

    Add code for exercise 5

---
+++ ex5-studydrill.py
@@ -0,0 +1,28 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# This line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+# Measurements
+centimetres_per_inch = 2.54
+kilos_per_lb = 0.453592
+
+# Print conversions
+print(f"He's {height * centimetres_per_inch} centimetres tall",
+      f"(rounded to {round(height * centimetres_per_inch)}).")
+print(f"He's {weight * kilos_per_lb} kilographs heavy",
+      f"(rounded to {round(weight * kilos_per_lb)}).")
