f66af770bece4743083a96c11d9fecf1187f78e4
Mike Bull <hello@mikebull.info>
2017-10-25 18:04:09 +0100

    Add code for exercise 9

---
+++ ex9-studydrill.py
@@ -0,0 +1,19 @@
+# Here's some strange stuff, remember type it exactly
+
+# Assign string of days to days variable
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Assign newline delimited months to months variable
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print days string
+print("Here are the days: ", days)
+# Print months string (\n will be replaced by new line in terminal)
+print("Here are the months: ", months)
+
+# Print multiline string
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
